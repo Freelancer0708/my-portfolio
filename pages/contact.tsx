090478dbec01f797b16cfe65cce426c7a7849320
Freelancer0708 <freelancernoaccont@gmail.com>
2023-05-03 15:42:11 +0900

    chenge mail server03 commit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,74 +1,55 @@
-// pages/contact.tsx
+import React, { useState } from "react";
+import axios from "axios";
 
-import { useState } from 'react';
-import Link from 'next/link';
-import ReCAPTCHA, { ReCAPTCHAChangeEvent } from 'react-google-recaptcha';
-
-const RECAPTCHA_SITE_KEY = '6LceiMMlAAAAAO5n26Uk_6lZkSUqZ-lTQRH9EmY4'; // 上で取得したサイトキーを入力
-
-const ContactPage = () => {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [recaptchaValue, setRecaptchaValue] = useState<string | null>(null);
+const Contact: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [result, setResult] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
-    if (!recaptchaValue) {
-      alert('reCAPTCHAの確認を行ってください。');
-      return;
-    }
-  
-    // APIルートを呼び
-    // APIルートを呼び出し、メールを送信
-    const response = await fetch('/api/send-email', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, message }),
-    });
+    try {
+      const response = await axios.post("/api/send-email", {
+        email,
+        message,
+      });
 
-    const data = await response.json();
-
-    if (response.status === 200) {
-        alert(data.message);
-        setEmail('');
-        setMessage('');
-    } else {
-        alert(`メールの送信に失敗しました。エラー: ${JSON.stringify(data.error)}`);
+      setResult(response.data.message);
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      setResult(`メールの送信に失敗しました。エラー: ${JSON.stringify(error.response.data)}`);
     }
   };
 
-    
-  
-
   return (
     <div>
-      <h1>Contact</h1>
+      <h1>お問い合わせフォーム</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">メールアドレス（必須）:</label>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <label>
+          メールアドレス（必須項目）:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
         <br />
-        <label htmlFor="message">お問い合わせ内容（必須）:</label>
-        <textarea
-          id="message"
-          value={message}
-          required
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <br />
-        <ReCAPTCHA sitekey={RECAPTCHA_SITE_KEY} onChange={(value) => setRecaptchaValue(value as string)} />
+        <label>
+          お問い合わせ内容（必須項目）:
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+        </label>
         <br />
         <button type="submit">送信</button>
       </form>
-      <Link href="/">Home</Link>
+      {result && <p>{result}</p>}
     </div>
   );
 };
 
-export default ContactPage;
+export default Contact;
