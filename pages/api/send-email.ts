629d692c1b42996c5dcba0ce2f37169c1ad7d6f3
Freelancer0708 <freelancernoaccont@gmail.com>
2023-05-04 03:32:22 +0900

    chenge mail server13 commit

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,62 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ message: "Method not allowed" });
-    return;
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    // Nodemailerのトランスポーターを設定
+    const transporter = nodemailer.createTransport({
+      host: 'sv10720.xserver.jp',
+      port: 465,
+      secure: true, // SSL/TLSを使用する場合はtrue
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    // メールの内容を設定
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: 'info@test-server00.xyz',
+      subject: 'お問い合わせ',
+      text: `名前: ${req.body.name}\nメールアドレス: ${req.body.email}\nお問い合わせ内容:\n${req.body.message}`,
+      replyTo: req.body.email,
+    };
+
+    // お問い合わせ者に自動返信メールの内容を設定
+    const autoReplyMailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: req.body.email,
+      subject: 'お問い合わせありがとうございます',
+      text: `以下の内容でお問い合わせを受け付けました。\n\n名前: ${req.body.name}\nメールアドレス: ${req.body.email}\nお問い合わせ内容:\n${req.body.message}\n\n追ってご連絡いたします。`,
+    };
+
+    // メールを送信
+    try {
+      await transporter.sendMail(mailOptions);
+      await transporter.sendMail(autoReplyMailOptions);
+      res.status(200).json({ status: 'Email sent' });
+    } catch (error) {
+      console.error('Email sending failed:', error);
+      res.status(500).json({ status: 'Email sending failed', error });
+    }
+  } else {
+    res.status(405).json({ status: 'Method not allowed' });
   }
-
-  const { email, message } = req.body;
-
-  // Configure mail transporter
-  const transporter = nodemailer.createTransport({
-    host: "sv10720.xserver.jp",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  // Set mail options for the recipient
-  const mailOptionsRecipient = {
-    from: process.env.EMAIL_USER,
-    to: "info@test-server00.xyz",
-    subject: "お問い合わせフォーム",
-    text: `
-      送信者: ${email}
-      お問い合わせ内容:
-      ${message}
-    `,
-  };
-
-  // Set mail options for the sender (auto-reply)
-  const mailOptionsSender = {
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "お問い合わせフォーム - 自動返信",
-    text: `
-      お問い合わせありがとうございます。
-      以下の内容でお問い合わせを受け付けました。
-
-      お問い合わせ内容:
-      ${message}
-
-      弊社からの返信をお待ちください。
-    `,
-  };
-
-  // Send emails
-  try {
-    await transporter.sendMail(mailOptionsRecipient);
-    await transporter.sendMail(mailOptionsSender);
-    res.status(200).json({ message: "メールが正常に送信されました。" });
-  } catch (error) {
-    console.error("Email send error:", error);
-    res.status(500).json({ message: "メールの送信に失敗しました。", error });
-  }
-};
+}
 
 export default handler;
